0cb78267ca380d5a9fd63af6087ea9ac77a76aaa
yixiangLuo <yixiangluo0111@gmail.com>
2016-11-20 21:57:49 -0500

    divide head file and source file

--- sample.c
+++ sample.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
 #include "bingham.h"
 
 int main(int argc, char** argv)
@@ -14,16 +13,27 @@ int main(int argc, char** argv)
 	binghamDataSpace* binghamDataPointer= initiateBingham();
 	
 	/* Calculation */
-	int i;
-	for(i=0;i<3;i++){
-		b[0]= (rand()/(double)(RAND_MAX));
-		b[1]= (rand()/(double)(RAND_MAX));
-		b[2]= (rand()/(double)(RAND_MAX));
-		
-		M = bingham(b, binghamDataPointer);
 	
-		printf("Z:\t\t%.10e\nZ(x1^2):\t%.10e\nZ(x2^2):\t%.10e\n<x1^2>:\t\t%.10e\n<x2^2>:\t\t%.10e\n<x1^2*x2^2>:\t%.10e\n<x1^4>:\t\t%.10e\n<x2^4>:\t\t%.10e\n\n", M.Z, M.Z_x12, M.Z_x22, M.Q_x12, M.Q_x22, M.Q_x12x22, M.Q_x14, M.Q_x24);
-	}
+	/* Example of case 2.1 in the paper */
+	b[0]= -50;
+	b[1]= -60;
+	b[2]= 0;	
+	M = bingham(b, binghamDataPointer);
+	printf("Z:\t\t%.10e\nZ(x1^2):\t%.10e\nZ(x2^2):\t%.10e\n<x1^2>:\t\t%.10e\n<x2^2>:\t\t%.10e\n<x1^2*x2^2>:\t%.10e\n<x1^4>:\t\t%.10e\n<x2^4>:\t\t%.10e\n\n", M.Z, M.Z_x12, M.Z_x22, M.Q_x12, M.Q_x22, M.Q_x12x22, M.Q_x14, M.Q_x24);
+	
+	/* Example of case 2.2 in the paper */
+	b[0]= -50;
+	b[1]= -10;
+	b[2]= 0;	
+	M = bingham(b, binghamDataPointer);
+	printf("Z:\t\t%.10e\nZ(x1^2):\t%.10e\nZ(x2^2):\t%.10e\n<x1^2>:\t\t%.10e\n<x2^2>:\t\t%.10e\n<x1^2*x2^2>:\t%.10e\n<x1^4>:\t\t%.10e\n<x2^4>:\t\t%.10e\n\n", M.Z, M.Z_x12, M.Z_x22, M.Q_x12, M.Q_x22, M.Q_x12x22, M.Q_x14, M.Q_x24);
+	
+	/* Example of case 2.3 in the paper */
+	b[0]= -10;
+	b[1]= -20;
+	b[2]= 0;	
+	M = bingham(b, binghamDataPointer);
+	printf("Z:\t\t%.10e\nZ(x1^2):\t%.10e\nZ(x2^2):\t%.10e\n<x1^2>:\t\t%.10e\n<x2^2>:\t\t%.10e\n<x1^2*x2^2>:\t%.10e\n<x1^4>:\t\t%.10e\n<x2^4>:\t\t%.10e\n\n", M.Z, M.Z_x12, M.Z_x22, M.Q_x12, M.Q_x22, M.Q_x12x22, M.Q_x14, M.Q_x24);
 	
 	/* Destroy the data structure and free the heap memory. */
 	binghamDataFree(binghamDataPointer);
